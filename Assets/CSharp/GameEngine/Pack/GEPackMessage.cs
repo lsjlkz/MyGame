370d51528c925e26aa15d7c8e21e3693c6e110af
夷则 <lsjlkz@gmail.com>
2023-05-11 18:07:12 +0800

    # 打包准备写

---
+++ Assets/CSharp/GameEngine/Pack/GEPackMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace CSharp
+{
+    public class GEPackMessage:GESingleton<GEPackMessage>
+    {
+        private bool isOK;
+        // 当前的流对象
+        private MemoryStream curBufPack;
+        private int curBufPackFence = 0;
+
+        public bool PackMsgType(UInt16 t)
+        {
+            // TODO 
+            curBufPack.Write(BitConverter.GetBytes(t), curBufPackFence, sizeof(UInt16));
+            return true;
+        }
+    }
+}
